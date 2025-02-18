0816d8e3e1b43fb496c713fb977800b0dd907387
soala144 <soala144@gmail.com>
2025-02-17 18:11:10 -0800

    1st phase of the UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-export default function App() {}
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
 
 // Logo Component
-function Logo() {}
+function Logo() {
+  return <h1>🌴 Far Away🚀</h1>;
+}
 
 // Logo Form
-function Form() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need😍 for your trip?</h3>
+    </div>
+  );
+}
 
 // Logo PackingList
-function PackingList() {}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
 
 // Logo Stats
-function Stats() {}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        You have have X items on your list, and you have already done X(X%)
+      </em>
+    </footer>
+  );
+}
